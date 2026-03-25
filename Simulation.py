7fa0a43e10c3f832a434388665ba3105e8305f41
Marwan <marwan.burhani@hotmail.com>
2026-03-24 18:39:50 -0600

    Bug Fixes + Fixing Pipeline -> Cleaner + Adjusting Hyperparameters since some computations were slightly different causing the model to not be as effective...

--- Simulation.py
+++ Simulation.py
@@ -3,40 +3,12 @@ import numpy as np
 import joblib
 import json
 from catboost import CatBoostClassifier
-from sklearn.metrics import confusion_matrix
-from utils import winsorize_fn, make_winsorizer, make_ratio, monthlycash, ratio_name
-from sklearn import set_config
-set_config(transform_output="default")
-
-#Loading model
-model = CatBoostClassifier()
-model.load_model('models/catboost_tuned.cbm')
-preprocessor  = joblib.load('models/preprocessor.pkl')
-
-
-with open('models/raw_features.json') as f:
-    raw_features = json.load(f)
-
-with open('models/config.json') as f:
-    config = json.load(f)
-
-threshold = config['threshold']
-lgd = config['lgd']
-avg_intRate = config['average_IntRate']
-avg_loan =config['avg_loan']
-
-
-import pandas as pd
-import numpy as np
-import joblib
-import json
-from catboost import CatBoostClassifier
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 from utils import winsorize_fn, make_winsorizer, make_ratio, monthlycash, ratio_name
 from sklearn import set_config
 set_config(transform_output="default")
 
-
+#Loading model
 model = CatBoostClassifier()
 model.load_model('models/catboost_tuned.cbm')
 preprocessor = joblib.load('models/preprocessor.pkl')
@@ -47,8 +19,6 @@ with open('models/raw_features.json') as f:
 with open('models/config.json') as f:
     config = json.load(f)
 
-with open('models/drop_features.json') as f:
-    features_todrop = json.load(f)
 
 threshold   = config['threshold']
 lgd         = config['lgd']
@@ -57,8 +27,7 @@ avg_loan    = config['avg_loan']
 
 
 
-def process_chunks(filepath, preprocessor, model, raw_features,
-                   features_todrop, threshold, chunksize=10_000):
+def process_chunks(filepath, preprocessor, model, raw_features, chunksize=10_000):
     all_proba = []
     all_true  = []
     all_X     = []
@@ -72,11 +41,7 @@ def process_chunks(filepath, preprocessor, model, raw_features,
         # Preprocess
         X_pre = preprocessor.transform(X_chunk)
         X_pre = pd.DataFrame(X_pre, columns=preprocessor.get_feature_names_out())
-
-        # Drop low SHAP features
-        X_pre = X_pre.drop(columns=[c for c in features_todrop if c in X_pre.columns])
-
-        
+    
         X_pre = X_pre[model_features]
 
         proba = model.predict_proba(X_pre)[:, 1]
@@ -92,9 +57,7 @@ def process_chunks(filepath, preprocessor, model, raw_features,
 
 y_proba, y_holdout, X_hold_pre = process_chunks(
     'Data/loan_data_holdout.csv',
-    preprocessor, model, raw_features,
-    features_todrop, threshold
-)
+    preprocessor, model, raw_features)
 
 
 print(f"Rows:          {len(y_holdout):,}")
